f24d98987b947a758c4c65d688df856a951c7eb3
benax-se <gyroskope225@gmail.com>
2023-08-24 11:11:35 +0300

    [gitlab] add review_app

--- bin/start.sh
+++ bin/start.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ "$NODE_ENV" = "production" ]; then
-  node lib/server.js
+  npx knex --knexfile config/knexfile.js migrate:latest && node lib/server.js
 else
   npx knex --knexfile config/knexfile.js migrate:latest
   npx nodemon lib/server.js | npx pino-pretty -c -t
